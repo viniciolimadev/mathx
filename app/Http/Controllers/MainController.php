691a29fd7416b0b4a894c34cbf0b77cdc0599e95
Vinicio <dev.vinicio@gmail.com>
2025-05-30 15:05:30 -0300

    Criando processamento de exercicios

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -27,6 +27,64 @@ class MainController extends Controller
 
         ]);
         
+        $operations = [];
+       if($request->check_sum) {$operations[] = 'sum';}
+       if($request->check_subtraction) { $operations[] =   'subtraction' ;}
+       if($request->check_multiplication){ $operations[] = 'multiplication';}
+       if($request->check_division){ $operations[] = 'division';}
+
+        $min = $request->number_one;
+        $max = $request->number_one;
+
+        $numberExercises = $request->number_exercises;
+
+        $exercises =[];
+        for ($index=1; $index <= $numberExercises ; $index++) { 
+            
+            $operation = $operations[array_rand($operations)];
+            $number1 = rand($min,$max);
+            $number2 = rand($min,$max);
+
+            $exercise = '';
+            $sollution = '';
+
+            switch ($operation) {
+                case 'sum':
+                    $exercise = "$number1 + $number2 =";
+                    $sollution = $number1 + $number2;
+                    break;
+                    case 'subtraction':
+                    $exercise = "$number1 - $number2 =";
+                    $sollution = $number1 - $number2;
+                    break;
+                    case 'multiplication':
+                    $exercise = "$number1 X $number2 =";
+                    $sollution = $number1 * $number2;
+                    break;
+                    case 'division':
+
+                    if($number2 == 0){
+                        $number2 = 1;
+                    }
+
+                    $exercise = "$number1 : $number2 =";
+                    $sollution = $number1 / $number2;
+                    break;
+            }
+
+            if(is_float($sollution)){
+               $sollution = round($sollution, 2);
+            }
+
+            $exercises[] = [
+                'operstion' => $operation,
+                'exercise_number' => $index,
+                'exercise' => $exercise,
+                'solution' => "$exercise $sollution"
+            ];
+        }
+        
+
     }
 
     public function printExercise(){
