2bf0828e0f9f6f152e84d9f1abcf67d67a957170
Vinicio <dev.vinicio@gmail.com>
2025-05-30 19:59:01 -0300

    Botão para fazer download do texto dos exercicios0

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Mail\Mailables\Content;
 
 class MainController extends Controller
 {
@@ -16,10 +17,14 @@ class MainController extends Controller
 
         //form validate
         $request->validate([
-            'check_sum' => 'required_without_all::check_sum,check_subtraction,check_multiplication,check_division',
-            'check_subtraction' => 'required_without_all::check_subtraction,check_sum,check_multiplication,check_division',
-            'check_multiplication' => 'required_without_all::check_multiplication,check_sum,check_subtraction,check_division',
-            'check_division' => 'required_without_all::check_division,check_sum,check_subtraction,check_multiplication',
+            'check_sum' =>
+            'required_without_all::check_sum,check_subtraction,check_multiplication,check_division',
+            'check_subtraction' => 
+            'required_without_all::check_subtraction,check_sum,check_multiplication,check_division',
+            'check_multiplication' => 
+            'required_without_all::check_multiplication,check_sum,check_subtraction,check_division',
+            'check_division' => 
+            'required_without_all::check_division,check_sum,check_subtraction,check_multiplication',
 
             'number_one' => 'required|integer|min:0|max:999',
             'number_two' => 'required|integer|min:0|max:999',
@@ -59,19 +64,47 @@ class MainController extends Controller
         echo '<hr>';
 
         foreach ($exercises as $exercise) {
-           echo '<h2><small>' . str_pad($exercise['exercise_number'], 2, "0", STR_PAD_LEFT) . ' >> </small> ' . $exercise['exercise'] . '</h2>';
+           echo '<h2><small>' . $exercise['exercise_number'] . ' >>
+            </small> ' . $exercise['exercise'] . '</h2>';
         }
         echo '<hr>';
         echo '<small>Soluções</small><br>';
         foreach ($exercises as $exercise) {
-           echo '<h2><small>' . str_pad($exercise['exercise_number'], 2, "0", STR_PAD_LEFT) . ' >> </small> ' . $exercise['sollution'] . '</h2>';
+           echo '<small><h3>' . $exercise['exercise_number'] . ' >> '
+            . $exercise['sollution'] . '</h3></small>';
         }
 
     }
     public function exportExercises(){
+        if(!session()->has('exercises')){
+            return redirect()->route('home');
+        }
+       // doenload dos exercicios
+         $exercises = session('exercises');
 
-        echo 'gerar exercicios';
-        
+        $filename = 'exercises_' . env('APP_NAME') . '_' . date('YmdHis') . '.txt';
+         
+        $content = '';
+
+        foreach ($exercises as $exercise) {
+
+            $content .= $exercise['exercise_number'] . ' > ' . $exercise['exercise'] . "\n";
+         }
+         
+         // soluções
+
+         $content .= "\n";
+         $content .= "Soluções"."\n" . str_repeat('-', 20) . "\n";
+
+         foreach ($exercises as $exercise) {
+
+            $content .= $exercise['exercise_number'] . ' > ' . $exercise['sollution'] . "\n";
+
+         }
+
+         return response($content)
+         ->header('Content-Type', 'text/plain')
+         ->header('Content-Disposition', 'attachment; filename="' . $filename . '"');
     }
 
     private function generateExercise($index,$operations,$min,$max):array{
@@ -82,6 +115,8 @@ class MainController extends Controller
 
             $exercise = '';
             $sollution = '';
+
+            if($number2 == 0){$number2 = 1;}
 
             switch ($operation) {
                 case 'sum':
@@ -97,11 +132,7 @@ class MainController extends Controller
                     $sollution = $number1 * $number2;
                     break;
                     case 'division':
-
-                    if($number2 == 0){
-                        $number2 = 1;
-                    }
-
+                    
                     $exercise = "$number1 : $number2 =";
                     $sollution = $number1 / $number2;
                     break;
@@ -112,8 +143,8 @@ class MainController extends Controller
             }
 
             return [
-                'operstion' => $operation,
-                'exercise_number' => $index,
+                'operation' => $operation,
+                'exercise_number' => str_pad($index, 2, "0", STR_PAD_LEFT),
                 'exercise' => $exercise,
                 'sollution' => "$exercise $sollution"
             ];
--- resources/views/operations.blade.php
+++ resources/views/operations.blade.php
@@ -30,7 +30,7 @@
 
             @foreach ($exercises as $exercise)
                 <div class="col-3 display-6 mb-3">
-                    <span class="badge bg-dark">{{ str_pad($exercise['exercise_number'], 2, '0',STR_PAD_LEFT) }}</span>
+                    <span class="badge bg-dark">{{($exercise['exercise_number'])}}</span>
                     <span>{{$exercise['exercise']}}</span>
                 </div>
             @endforeach
