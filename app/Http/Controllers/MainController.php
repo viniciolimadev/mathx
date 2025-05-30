ccb794c66943f9470cac3a6dfa3fe1bfaf070795
Vinicio <dev.vinicio@gmail.com>
2025-05-30 12:19:15 -0300

    Criando a view home

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function home(){
+    public function home():View{
 
-        echo 'apresentar a pagina inicial';
+        return view('home');
         
     }
     public function generateExercise(Request $request){
